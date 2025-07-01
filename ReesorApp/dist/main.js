30c6876d22d35e75d403e830032e63db9488c000
Michael Harris <158084149+Mikemike8@users.noreply.github.com>
2025-07-01 14:49:42 -0500

    build fix

--- ReesorApp/dist/main.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { ErrorBoundary } from './components/ErrorBoundary.tsx';
-import './index.css';
-import App from './App.tsx';
-createRoot(document.getElementById('root')).render(_jsx(StrictMode, { children: _jsx(ErrorBoundary, { children: _jsx(App, {}) }) }));
